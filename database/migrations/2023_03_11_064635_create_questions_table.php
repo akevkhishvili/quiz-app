562f5683b999a322e131e0dd0b1831202d090a03
akevkhishvili <akevkhishvili@cu.edu.ge>
2023-03-12 19:10:24 +0400

    first commit

---
+++ database/migrations/2023_03_11_064635_create_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\QuestionMode;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('question');
+            $table->foreignIdFor(QuestionMode::class)
+                ->constrained()
+                ->CascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->integer('correct_answer_id');
+            $table->timestamps();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
